ac16fc11ffdd0da707bbb85e78c53492e64030a6
leonardomarchioro <leonardomarchioro11@gmail.com>
2022-03-17 11:08:22 -0300

    feat: logo add to home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import ModalLogin from "../../components/ModalLogin";
+import Logo from "../../assets/Logo.png";
 
 const Home = () => {
   return (
     <div>
+      <img src={Logo} alt="Logo" />
       <ModalLogin />
     </div>
   );

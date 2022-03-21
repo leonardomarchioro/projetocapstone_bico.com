9c6654428d880486cfae662c421a55d8c6387150
GabrielRD <grd342@outlook.com>
2022-03-21 10:46:49 -0300

    feat:component services

---
+++ src/MyServicesComponent/index.jsx
@@ -0,0 +1,25 @@
+import Button from "../components/Button";
+import { useService } from "../providers/Services";
+
+const MyServicesComponent = ({ service }) => {
+  const { deleteService, attServiceReview } = useService();
+
+  return (
+    <li key={service.id} id={service.id}>
+      <h2>{service.category}</h2>
+      <span>{service.dateActual}</span>
+      <Button text="Excluir" onClick={() => deleteService(service.id)} />
+      <Button
+        text="review"
+        onClick={() =>
+          attServiceReview(service.id, {
+            score: 5,
+            comment: " trabalha direitinho o rapaz",
+          })
+        }
+      />
+    </li>
+  );
+};
+
+export default MyServicesComponent;
--- src/components/MyServices/index.jsx
+++ src/components/MyServices/index.jsx
@@ -1,11 +1,9 @@
 import { useService } from "../../providers/Services";
-import { useEffect, useState } from "react";
-import Button from "../Button";
-import { dateFormated } from "../../uteils";
+import { useEffect } from "react";
+import MyServicesComponent from "../../MyServicesComponent";
 
 const MyServices = () => {
-  const { getSevicesClient, services, deleteService, attServiceReview } =
-    useService();
+  const { getSevicesClient, services } = useService();
 
   console.log(services);
 
@@ -18,25 +16,7 @@ const MyServices = () => {
     <div>
       <ul>
         {services.map((service) => {
-          return (
-            <li key={service.id} id={service.id}>
-              <h2>{service.category}</h2>
-              <span>{service.dateActual}</span>
-              <Button
-                text="Excluir"
-                onClick={() => deleteService(service.id)}
-              />
-              <Button
-                text="review"
-                onClick={() =>
-                  attServiceReview(service.id, {
-                    score: 5,
-                    comment: " trabalha direitinho o rapaz",
-                  })
-                }
-              />
-            </li>
-          );
+          return <MyServicesComponent service={service} />;
         })}
       </ul>
     </div>

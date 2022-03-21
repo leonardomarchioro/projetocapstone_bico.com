45111b77ceabac32d34b7c75babac32ef3b79e23
Luiz Felipe OP <luiz.felipe221295@hotmail.com>
2022-03-21 14:49:33 -0300

    ended modal styles

--- src/components/ModalConfirmation/index.jsx
+++ src/components/ModalConfirmation/index.jsx
@@ -1,13 +1,46 @@
 import { Container } from "./styles";
+import { motion } from "framer-motion";
+import { FiCheckSquare, FiXSquare } from "react-icons/fi";
 
 const ModalConfirmation = () => {
+  const dropIn = {
+    hidden: {
+      y: "-100vh",
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.1,
+        type: "spring",
+        damping: 100,
+        stiffness: 500,
+      },
+    },
+    exit: {
+      y: "100vh",
+      opacity: 0,
+    },
+  };
   return (
     <Container>
-      <div>
+      <motion.div
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
         <h2>Tem certeza que quer se tornar membro ?</h2>
-        <button>Sim</button>
-        <button>Nao</button>
-      </div>
+        <div>
+          <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <FiCheckSquare size={35} />
+          </motion.button>
+          <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <FiXSquare size={35} />
+          </motion.button>
+        </div>
+      </motion.div>
     </Container>
   );
 };

a3146bc7fc0143e32a36c3788702ac276a991a48
leonardomarchioro <leonardomarchioro11@gmail.com>
2022-03-22 10:20:36 -0300

    merge from Styles/StyleService

--- src/components/DashboardSuplier/styles.js
+++ src/components/DashboardSuplier/styles.js
@@ -6,36 +6,26 @@ export const Container = styled.div`
   flex-wrap: wrap;
   justify-content: flex-start;
   padding: 15px 15px 0px 15px;
-  max-width: 1000px;
+  min-width: 300px;
   min-height: 138px;
 
-  h3 {
+  h2 {
     font-size: 16px;
     font-weight: 500;
     padding-bottom: 2px;
+    background: #78bced;
+    border-radius: 5px 5px 0 0;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #1e637d;
+    min-width: 300px;
   }
-  span {
-    font-size: 12px;
-    font-weight: 500;
-    padding-bottom: 2px;
-    color: #003347;
-  }
-  p {
+  ul {
+    background-color: #4f718d;
+    border-radius: 0 0 5px 5px;
+    padding: 10px;
+    min-width: 300px;
   }
 `;
-
-// export const DivPar = styled.p`
-// word-wrap: break-word;
-// font-size: 12px;
-//     font-weight: 500;
-//     color: #003347;
-//     max-width: 350px;
-//     max-height: 50px;
-//     margin-bottom: 15px;
-// `
-
-// export const DivPai = styled.div`
-// display: flex;
-// flex-direction: row;
-
-// `

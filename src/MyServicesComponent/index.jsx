3c9f87dad60a7759c7f12f2d557ac28991c6bb92
leonardomarchioro <leonardomarchioro11@gmail.com>
2022-03-21 21:45:24 -0300

    feat: verification if services and suppliers

--- src/MyServicesComponent/index.jsx
+++ src/MyServicesComponent/index.jsx
@@ -1,24 +1,37 @@
 import Button from "../components/Button";
 import { useService } from "../providers/Services";
+import ModalInfoMyService from "../components/ModalInfoMyService";
+import { useState } from "react";
 
 const MyServicesComponent = ({ service }) => {
   const { deleteService, attServiceReview } = useService();
-
+  const [showModalInfo, setShowModalInfo] = useState(false);
+  console.log(service.supplier);
   return (
-    <li key={service.id} id={service.id}>
-      <h2>{service.category}</h2>
-      <span>{service.dateActual}</span>
-      {/* <Button text="X" onClick={() => deleteService(service.id)} />
-      <Button
-        text="+"
-        onClick={() =>
-          attServiceReview(service.id, {
-            score: 5,
-            comment: " trabalha direitinho o rapaz",
-          })
-        }
-      /> */}
-    </li>
+    <>
+      {showModalInfo && (
+        <ModalInfoMyService
+          service={service}
+          setShowModalInfo={setShowModalInfo}
+        />
+      )}
+      {service.supplier.length > 0 ? (
+        <li
+          className="Candidato"
+          key={service.id}
+          id={service.id}
+          onClick={() => setShowModalInfo(true)}
+        >
+          <h2>{service.category}</h2>
+          <span>{service.dateActual}</span>
+        </li>
+      ) : (
+        <li key={service.id} id={service.id}>
+          <h2>{service.category}</h2>
+          <span>{service.dateActual}</span>
+        </li>
+      )}
+    </>
   );
 };
 
--- src/components/DashboardSuplier/index.jsx
+++ src/components/DashboardSuplier/index.jsx
@@ -4,17 +4,17 @@ import { useEffect } from "react";
 import ContainerInfo from "../DashSuplierInfo";
 
 const DashboardSuplier = () => {
-  const { getAllServices, allServices } = useService();
+  const { getAllServices, availableServices } = useService();
 
   useEffect(() => {
     getAllServices();
   }, []);
-  // console.log(allServices);
+
   return (
     <Container>
       <h2>PÁGINA DE ANÚNCIOS</h2>
       <ul>
-        {allServices.map((service) => {
+        {availableServices.map((service) => {
           return <ContainerInfo key={service.id} service={service} />;
         })}
       </ul>
